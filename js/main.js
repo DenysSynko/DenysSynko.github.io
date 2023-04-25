1b953c5e5b45a39bba9f7af1ad521a6a6db9e237
Denys Synko <denissinko2004@gmail.com>
2023-04-25 09:56:10 +0300

    js1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var app = new Vue({
     el:".container2",
     data:{
-        products:[{'id':1, 'title':'TAG 1000 (TAG 853)','image':'image_2.jpg', 'short_text': 'Kiwi Gold', 'desc': 'Full description'},
-        {'id':2, 'title':'TAG 1001 (TAG 855)','image':'image_3.jpg', 'short_text': 'Kiwi Green', 'desc': 'Full description'},
-        {'id':3, 'title':'TAG 1002 (TAG 809)','image':'image_4.jpg', 'short_text': 'Kiwi Green', 'desc': 'Full description'},
-        {'id':4, 'title':'TAG 1003 (TAG 834 )','image':'image_5.jpg', 'short_text': 'Actinidia', 'desc': 'Full description'},
-        {'id':5, 'title':'TAG 1004 (TAG 848)','image':'image_6.jpg', 'short_text': 'Zespri Red Kiwi', 'desc': 'Full description'}],
+        products:[{'id':1, 'title':'TAG 1000 (TAG 853)','image':'image_2.jpg', 'short_text': 'American Giant', 'desc': 'Full description'},
+        {'id':2, 'title':'TAG 1001 (TAG 855)','image':'image_3.jpg', 'short_text': 'Arnika', 'desc': 'Full description'},
+        {'id':3, 'title':'TAG 1002 (TAG 809)','image':'image_4.jpg', 'short_text': 'Autumn Beauty', 'desc': 'Full description'},
+        {'id':4, 'title':'TAG 1003 (TAG 834 )','image':'image_5.jpg', 'short_text': 'Aztec Sun', 'desc': 'Full description'},
+        {'id':5, 'title':'TAG 1004 (TAG 848)','image':'image_6.jpg', 'short_text': 'Evening Sun', 'desc': 'Full description'}],
         
     }
 });
